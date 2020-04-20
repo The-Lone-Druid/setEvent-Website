58687f8d6262c02b7e93b4acf0bd3ec834c6a162
Zahid Ibrahim Shaikh <53161242+The-Lone-Druid@users.noreply.github.com>
2020-04-20 10:09:54 +0530

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+var mobileNumber = document.getElementById('mobileNumber');
+
+    if(mobileNumber > 10) {
+        document.getElementById('display').innerHTML = 'Contact no. more than 10 digits';
+    }
